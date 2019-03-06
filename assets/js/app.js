be40a37a865b7550d69313b06314685ee442567c
Pablo Fernandez <pablofcasal@mail.com>
2019-03-07 00:26:40 +0100

    Added delete tweet button

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,9 @@ function eventListeners() {
      
     // When the form is submmitted
     document.querySelector('#formulario').addEventListener('submit', addTweet);
+
+    // Delete tweets
+    tweetsList.addEventListener('click', deleteTweet);
     
 
 };
@@ -19,15 +22,16 @@ function eventListeners() {
 
 // Add tweet from the form
 function addTweet(e) {
-    e.preventDefault(e);
+    e.preventDefault();
     
     // Read tweet content
     const tweet = document.getElementById('tweet').value;
 
+
     // Delete tweet button
     const deleteButton = document.createElement('a');
     deleteButton.classList = 'delete-tweet';
-    deleteButton.innerText = 'X';
+    deleteButton.innerText = 'X';  
 
 
     // Tweet creation and adiction to the tweets list
@@ -38,9 +42,17 @@ function addTweet(e) {
     li.appendChild(deleteButton);
     tweetsList.appendChild(li);
 
+};
 
+function deleteTweet(e){
 
-
+    e.preventDefault();
+    if(e.target.className === 'delete-tweet') {
+        alert('Tweet aliminado');
+    }
     
+    
+
+
 };
  
